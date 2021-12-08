53152ea1355e249efc566986dd5c3ec1f1247d3b
miodzie <miodzie@protonmail.com>
2021-12-07 19:48:56 -0500

    Simple File Components, neat.

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import HelloWorld from './components/HelloWorld.vue'
+import SearchBox from './components/SearchBox.vue'
 </script>
 
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
+  <SearchBox />
   <HelloWorld msg="Hello Vue 3 + Vite" />
 
 </template>
---
+++ src/components/SearchBox.vue
@@ -0,0 +1,16 @@
+<template>
+  <button type="button" @click="hello" 
+    class="p-2  bg-white rounded-sm shadow-md ">
+    Create Link
+  </button>
+</template>
+
+<script>
+export default {
+  methods: {
+    hello() {
+      alert('hello world!')
+    }
+  }
+}
+</script>
