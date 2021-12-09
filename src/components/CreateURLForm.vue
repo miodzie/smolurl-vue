15578841b6286d5f108758f5b3a3d2fa4c93d5bf
miodzie <miodzie@protonmail.com>
2021-12-09 12:38:05 -0500

    Alright, tailwindcss is pretty cool.

---
+++ src/components/CreateURLForm.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="flex flex-col max-w-xs p-8 m-auto space-y-4 bg-gray-50 rounded
+    border border-gray-300">
+    <input 
+      class="border border-gray-300 rounded bg-gray-50 focus:bg-white"
+      v-model="url" />
+    <button type="button" 
+      class="p-3 bg-green-500 hover:bg-green-600 active:bg-green-400 text-white rounded font-mono text-sm font-semibold border-black shadow-md">
+      Create Link
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      url: '',
+    };
+  },
+  methods: {
+    hello() {
+      alert('hello world!')
+    }
+  }
+}
+</script>
+
--- src/components/SearchBox.vue
+++ src/components/SearchBox.vue
@@ -7,6 +7,11 @@
 
 <script>
 export default {
+  data: function() {
+    return {
+      
+    };
+  },
   methods: {
     hello() {
       alert('hello world!')
